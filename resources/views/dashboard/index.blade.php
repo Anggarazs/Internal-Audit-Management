e17eda551d33bc8df44de3dd5754a8efb7d98e6f
erzaffasya <58721335+erzaffasya@users.noreply.github.com>
2021-11-04 12:45:02 +0700

    a

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,214 +1,203 @@
 @extends('layouts.master')
 @push('css')
-    
-	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
 
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	
-	<link rel="stylesheet" href="{{asset('calender/css/style.css')}}">
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<link rel="stylesheet" href="{{asset('calender/css/style.css')}}">
 @endpush
 @section('content')
 <div class="container-fluid">
 
-<!-- Page Heading -->
-<div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h3 mb-0 text-gray-800 ">Dashboard</h1>
-</div>
-
-<!-- Content Row -->
-<div class="row">
-
-    <!-- Earnings (Monthly) Card Example -->
-    <div class="col-xl-4 col-md-6 mb-4">
-        <div class="card border-left-primary shadow h-100 py-2">
-            <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                            Total Audit List</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">null</div>
-                    </div>
-                    <div class="col-auto">
-                        <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800 ">Dashboard</h1>
     </div>
 
-    <!-- Earnings (Monthly) Card Example -->
-    <div class="col-xl-4 col-md-6 mb-4">
-        <div class="card border-left-success shadow h-100 py-2">
-            <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                            Total Pica Audit</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">null</div>
-                    </div>
-                    <div class="col-auto">
-                        <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <!-- Content Row -->
+    <div class="row">
 
-    <!-- Earnings (Monthly) Card Example -->
-    <div class="col-xl-4 col-md-6 mb-4">
-        <div class="card border-left-info shadow h-100 py-2">
-            <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
-                            Perbaikan Audit
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-4 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                Total Audit List</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">null</div>
                         </div>
-                        <div class="row no-gutters align-items-center">
-                            <div class="col-auto">
-                                <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                        <div class="col-auto">
+                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-4 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                Total Pica Audit</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">null</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-4 col-md-6 mb-4">
+            <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                                Perbaikan Audit
                             </div>
-                            <div class="col">
-                                <div class="progress progress-sm mr-2">
-                                    <div class="progress-bar bg-info" role="progressbar"
-                                        style="width: 50%" aria-valuenow="50" aria-valuemin="0"
-                                        aria-valuemax="100"></div>
+                            <div class="row no-gutters align-items-center">
+                                <div class="col-auto">
+                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                                </div>
+                                <div class="col">
+                                    <div class="progress progress-sm mr-2">
+                                        <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-auto">
-                        <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                        <div class="col-auto">
+                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<!-- Content Row -->
-<div class="row">
+    <!-- Content Row -->
+    <div class="row">
 
-    <!-- Pie Chart -->
-    <div class="col-xl-4 col-lg-5">
-        <div class="card shadow mb-4">
-            <!-- Card Header - Dropdown -->
-            <div
-                class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                <h6 class="m-0 font-weight-bold text-primary">Corrective Action Plan Summary</h6>
-                <div class="dropdown no-arrow">
-                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-                        aria-labelledby="dropdownMenuLink">
-                        <div class="dropdown-header">Dropdown Header:</div>
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
+        <!-- Pie Chart -->
+        <div class="col-xl-4 col-lg-5">
+            <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary">Corrective Action Plan Summary</h6>
+                    <div class="dropdown no-arrow">
+                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
+                            <div class="dropdown-header">Dropdown Header:</div>
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- Card Body -->
+                <div class="card-body">
+                    <div class="chart-pie pt-4 pb-2">
+                        <canvas id="myPieChart"></canvas>
+                    </div>
+                    <div class="mt-4 text-center small">
+                        <span class="mr-2">
+                            <i class="fas fa-circle text-secondary"></i> On Progress
+                        </span>
+                        <span class="mr-2">
+                            <i class="fas fa-circle text-danger"></i> Closed
+                        </span>
                     </div>
                 </div>
             </div>
-            <!-- Card Body -->
-            <div class="card-body">
-                <div class="chart-pie pt-4 pb-2">
-                    <canvas id="myPieChart"></canvas>
-                </div>
-                <div class="mt-4 text-center small">
-                    <span class="mr-2">
-                        <i class="fas fa-circle text-secondary"></i> On Progress
-                    </span>
-                    <span class="mr-2">
-                        <i class="fas fa-circle text-danger"></i> Closed
-                    </span>
-                </div>
-            </div>
         </div>
-    </div>
-</div>
 
-<section class="ftco-section">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-6 text-center mb-5">
-                <h2 class="heading-section">Calendar #04</h2>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <div class="content w-100">
-                <div class="calendar-container">
-                  <div class="calendar"> 
-                    <div class="year-header"> 
-                      <span class="left-button fa fa-chevron-left" id="prev"> </span> 
-                      <span class="year" id="label"></span> 
-                      <span class="right-button fa fa-chevron-right" id="next"> </span>
-                    </div> 
-                    <table class="months-table w-100"> 
-                      <tbody>
-                        <tr class="months-row">
-                          <td class="month">Jan</td> 
-                          <td class="month">Feb</td> 
-                          <td class="month">Mar</td> 
-                          <td class="month">Apr</td> 
-                          <td class="month">May</td> 
-                          <td class="month">Jun</td> 
-                          <td class="month">Jul</td>
-                          <td class="month">Aug</td> 
-                          <td class="month">Sep</td> 
-                          <td class="month">Oct</td>          
-                          <td class="month">Nov</td>
-                          <td class="month">Dec</td>
-                        </tr>
-                      </tbody>
-                    </table> 
-                    
-                    <table class="days-table w-100"> 
-                      <td class="day">Sun</td> 
-                      <td class="day">Mon</td> 
-                      <td class="day">Tue</td> 
-                      <td class="day">Wed</td> 
-                      <td class="day">Thu</td> 
-                      <td class="day">Fri</td> 
-                      <td class="day">Sat</td>
-                    </table> 
-                    <div class="frame"> 
-                      <table class="dates-table w-100"> 
-                      <tbody class="tbody">             
-                      </tbody> 
-                      </table>
-                    </div> 
-                    <button class="button" id="add-button">Add Event</button>
-                  </div>
+
+        <div class="col-xl-8 col-lg-6">
+            <div class="card shadow mb-4">
+                <div class="col-md-12">
+                    <div class="content w-100">
+                        <div class="calendar-container">
+                            <div class="calendar">
+                                <div class="year-header">
+                                    <span class="left-button fa fa-chevron-left" id="prev"> </span>
+                                    <span class="year" id="label"></span>
+                                    <span class="right-button fa fa-chevron-right" id="next"> </span>
+                                </div>
+                                <table class="months-table w-100">
+                                    <tbody>
+                                        <tr class="months-row">
+                                            <td class="month">Jan</td>
+                                            <td class="month">Feb</td>
+                                            <td class="month">Mar</td>
+                                            <td class="month">Apr</td>
+                                            <td class="month">May</td>
+                                            <td class="month">Jun</td>
+                                            <td class="month">Jul</td>
+                                            <td class="month">Aug</td>
+                                            <td class="month">Sep</td>
+                                            <td class="month">Oct</td>
+                                            <td class="month">Nov</td>
+                                            <td class="month">Dec</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+
+                                <table class="days-table w-100">
+                                    <td class="day">Sun</td>
+                                    <td class="day">Mon</td>
+                                    <td class="day">Tue</td>
+                                    <td class="day">Wed</td>
+                                    <td class="day">Thu</td>
+                                    <td class="day">Fri</td>
+                                    <td class="day">Sat</td>
+                                </table>
+                                <div class="frame">
+                                    <table class="dates-table w-100">
+                                        <tbody class="tbody">
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <button class="button" id="add-button">Add Event</button>
+                            </div>
+                        </div>
+                        <div class="events-container">
+                        </div>
+                        <div class="dialog" id="dialog">
+                            <h2 class="dialog-header"> Add New Event </h2>
+                            <form class="form" id="form">
+                                <div class="form-container" align="center">
+                                    <label class="form-label" id="valueFromMyButton" for="name">Event name</label>
+                                    <input class="input" type="text" id="name" maxlength="36">
+                                    <label class="form-label" id="valueFromMyButton" for="count">Number of people to invite</label>
+                                    <input class="input" type="number" id="count" min="0" max="1000000" maxlength="7">
+                                    <input type="button" value="Cancel" class="button" id="cancel-button">
+                                    <input type="button" value="OK" class="button button-white" id="ok-button">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                <div class="events-container">
-                </div>
-                <div class="dialog" id="dialog">
-                    <h2 class="dialog-header"> Add New Event </h2>
-                    <form class="form" id="form">
-                      <div class="form-container" align="center">
-                        <label class="form-label" id="valueFromMyButton" for="name">Event name</label>
-                        <input class="input" type="text" id="name" maxlength="36">
-                        <label class="form-label" id="valueFromMyButton" for="count">Number of people to invite</label>
-                        <input class="input" type="number" id="count" min="0" max="1000000" maxlength="7">
-                        <input type="button" value="Cancel" class="button" id="cancel-button">
-                        <input type="button" value="OK" class="button button-white" id="ok-button">
-                      </div>
-                    </form>
-                  </div>
-              </div>
+
             </div>
         </div>
     </div>
-</section>
-    
 </div>
 
 @push('script')
-    
-	<script src="{{asset('calender/js/jquery.min.js')}}"></script>
-    <script src="{{asset('calender/js/popper.js')}}"></script>
-    <script src="{{asset('calender/js/bootstrap.min.js')}}"></script>
-    <script src="{{asset('calender/js/main.js')}}"></script>
+
+<script src="{{asset('calender/js/jquery.min.js')}}"></script>
+<script src="{{asset('calender/js/popper.js')}}"></script>
+<script src="{{asset('calender/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('calender/js/main.js')}}"></script>
 @endpush
 @endsection
