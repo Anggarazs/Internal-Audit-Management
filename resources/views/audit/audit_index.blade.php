561f2d84f3b7145d7c5e98db1d715be425f23e00
Afiery <56961591+Afiery@users.noreply.github.com>
2021-11-09 14:03:25 +0800

    Update

--- resources/views/audit/audit_index.blade.php
+++ resources/views/audit/audit_index.blade.php
@@ -19,7 +19,7 @@
 <!-- DataTales Example -->
 <div class="card shadow mb-4">
 <div class="card-header py-3">
-    <h6 class="m-0 font-weight-bold text-primary">Data Laporan Audit</h6>
+    <h6 class="m-0 font-weight-bold text-primary">Data Audit Report</h6>
 </div>
 <div class="card-body">
         <a href="#" class="btn mb-3 btn-success btn-icon-split btn-sm" data-toggle="modal" data-target="#insertModal">
@@ -29,45 +29,47 @@
         <span class="text">Tambah Data Laporan Audit</span>
     </a>
     <div class="table-responsive">
-        <table class="table table-bordered" id="myTable" width="100%" cellspacing="0">
+        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
             <thead>
                 <tr class="text-center">
-                    <th >No</th>
                     <th>No Laporan Audit</th>
                     <th>Judul Laporan</th>
                     <th>Tipe Audit</th>
                     <th>Jenis Audit</th>
+                    <th>Objek</th>
                     <th>Auditor</th>
+                    <th>Department</th>
                     <th>Kriteria Audit</th>
                     <th>Tahun Audit</th>
                     <th>Tanggal Mulai</th>
                     <th>Tanggal Akhir</th>
+                    <th>Jumlah Temuan</th>
                     <th>Evidence</th>
                     <th>Edit</th>
                     <th>Delete</th>
                 </tr>
             </thead>
             <tbody>
-            @php
-                $i = 1;
-            @endphp
+    
             @foreach($audit as $list_audit)
                 <tr class="text-center">
-                    <td>{{ $i++ }}</td>
-                    <td>{{ $list_audit -> no_audit }}</td>
+                    <td>{{ $list_audit -> no_laporan_audit }}</td>
                     <td>{{ $list_audit -> judul_audit }}</td>
                     <td>{{ $list_audit -> tipe_audit }}</td>
                     <td>{{ $list_audit -> jenis_audit }}</td>
+                    <td>{{ $list_audit -> objek}}</td>
                     <td>{{ $list_audit -> auditor }}</td>
+                    <td>{{ $list_audit -> department }}</td>
                     <td>{{ $list_audit -> kriteria_audit }}</td>
                     <td>{{ $list_audit -> tahun_audit }}</td>
                     <td>{{ $list_audit -> tanggal_mulai_audit }}</td>
                     <td>{{ $list_audit -> tanggal_akhir_audit  }}</td>
-                    <td >
+                    <td>{{ $list_audit -> jumlah_temuan }}</td>
+                    <td>
                     <a target="_blank" href="{{url('storage/LaporanAudit', $list_audit -> file)}}">{{ $list_audit -> file }}</a>
                     </td>
                     <td class="text-center">
-                    <a class="btn btn-info btn-icon-split btn-sm" type="submit">
+                    <a href="edit_audit/{{$list_audit->no_audit}}" class="btn btn-info btn-icon-split btn-sm" type="submit">
                         <span class="icon text-white-50">
                             <i class="fas  fa-edit"></i>
                         </span>
@@ -75,7 +77,7 @@
                     </a>  
                     </td>
                     <td class="text-center">
-                        <a href="delete_audit/{{$list_audit->no_audit}}"  class="btn btn-danger btn-icon-split btn-sm" type="submit">
+                        <a href="delete_audit/{{$list_audit->no_audit}}" onclick="return confirm('Apakah anda yakin ingin menghapus laporan audit?')" class="btn btn-danger btn-icon-split btn-sm" type="submit">
                             <span class="icon text-white-50">
                                 <i class="fas fa-trash"></i>
                             </span>
@@ -103,8 +105,8 @@
                 @csrf
                 <div class="form-group row">
                     <div class="col-sm-6">
-                        <input id="no_audit" type="text" class="form-control form-control-user"  name="no_audit" value="{{ old('no_audit') }}" required autocomplete="no_audit" placeholder="Nomor Laporan Audit" readonly>
-                        @error('no_audit')
+                        <input id="no_laporan_audit" type="text" class="form-control form-control-user"  name="no_laporan_audit" value="{{ old('no_laporan_audit') }}" required autocomplete="no_laporan_audit" placeholder="Nomor Laporan Audit">
+                        @error('no_laporan_audit')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
                             </span>
@@ -143,12 +145,10 @@
                         @enderror
                     </div>
                     <div class="col-sm-6">
-                        <select name="jenis_audit" class=" form-control form-control-user @error('jenis_audit') is-invalid @enderror" id="jenis_audit" required>
-                            <option value=""selected >Pilih Jenis Audit</option>                 
-                            <option value="Jenis 1">Jenis 1</option>
-                            <option value="Jenis 2">Jenis 2</option> 
-                            <option value="Jenis 3">Jenis 3</option>
-                            <option value="Jenis 4">Jenis 4</option>                  
+                        <select name="jenis_audit[]" class="selectpicker form-control form-control-user @error('jenis_audit') is-invalid @enderror"  multiple data-live-search="true" placeholder="Pilih Jenis Audit" required>               
+                            @foreach ($jenis_audit as $list_jenis_audit)
+                            <option value="{{$list_jenis_audit->id_jenis_audit}}" @if(in_array($list_jenis_audit->id_jenis_audit,old('jenis_audit',[]))) @endif>{{ $list_jenis_audit->jenis_audit}}</option>
+                            @endforeach               
                         </select>
                          @error('jenis_audit')
                             <span class="invalid-feedback" role="alert">
@@ -159,7 +159,25 @@
                 </div>
                 <div class="form-group row">
                     <div class="col-sm-6">
-                        <input id="kriteria_audit" type="checbox" class="form-control form-control-user"  name="kriteria_audit" value="{{ old('kriteria_audit') }}" required autocomplete="kriteria_audit" placeholder="Kriteria Audit">
+                        <input id="objek" type="text" class="form-control form-control-user"  name="objek" value="{{ old('objek') }}" required autocomplete="objek" placeholder="Objek">
+                        @error('objek')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="col-sm-6">
+                        <input id="department" type="text" class="form-control form-control-user"  name="department" value="{{ old('department') }}" required autocomplete="department" placeholder="Department">
+                        @error('department')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>      
+                <div class="form-group row">
+                    <div class="col-sm-6">
+                        <input id="kriteria_audit" type="text" class="form-control form-control-user"  name="kriteria_audit" value="{{ old('kriteria_audit') }}" required autocomplete="kriteria_audit" placeholder="Kriteria Audit">
                         @error('kriteria_audit')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
@@ -187,6 +205,16 @@
                     <div class="col-sm-6">
                      <input id="tanggal_akhir_audit" type="text" class="form-control form-control-user" onfocus="(this.type='date')" onblur="(this.type='text')" name="tanggal_akhir_audit" required autocomplete="tanggal_akhir_audit" placeholder="Tanggal Akhir">
                       @error('tanggal_akhir_audit')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-sm-6">
+                        <input id="jumlah_temuan" type="text" class="form-control form-control-user"  name="jumlah_temuan" value="{{ old('jumlah_temuan') }}" required autocomplete="jumlah_temuan" placeholder="Jumlah Temuan">
+                        @error('jumlah_temuan')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
                             </span>
