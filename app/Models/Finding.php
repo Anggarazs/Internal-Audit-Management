a4ac057990e2a022c07b9ef10973cad3c5189226
Afiery <56961591+Afiery@users.noreply.github.com>
2021-11-15 11:34:35 +0800

    rename dan update dari Corrective ke Finding

---
+++ app/Models/Finding.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Finding extends Model
+{
+    use HasFactory;
+    protected $table = 'finding';
+    protected $primaryKey = 'id_finding';
+    protected $fillable = ['id_finding','no_laporan_audit','judul_audit','status','progress','tipe_audit','risk_level','kriteria_audit','tahun_audit','tanggal_mulai_audit','tanggal_akhir_audit','auditor','finding','root','department','auditee','corrective','due_date','file'];
+}
