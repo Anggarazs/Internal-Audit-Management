04aff5bae2d33e9344301f063c7428492ca8addf
Afiery <56961591+Afiery@users.noreply.github.com>
2021-11-15 12:07:57 +0800

    Update finding

--- app/Models/Finding.php
+++ app/Models/Finding.php
@@ -11,4 +11,11 @@ class Finding extends Model
     protected $table = 'finding';
     protected $primaryKey = 'id_finding';
     protected $fillable = ['id_finding','no_laporan_audit','judul_audit','status','progress','tipe_audit','risk_level','kriteria_audit','tahun_audit','tanggal_mulai_audit','tanggal_akhir_audit','auditor','finding','root','department','auditee','corrective','due_date','file'];
+
+    public function laporan_audit(){
+        return $this->belongsTo(Audit::class,'id_audit','no_audit');
+
+    }
+
+  
 }
